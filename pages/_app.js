5c17dd7663a0c9ba05adac0b7f2912305b31d639
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-11-26 21:35:27 +0800

    add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
