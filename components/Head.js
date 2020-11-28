4aed3d66be6555af3e19d07cdfe1fb58a27fb017
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-11-28 22:15:01 +0800

    add meta description

--- components/Head.js
+++ components/Head.js
@@ -1,12 +1,15 @@
 import NextHead from 'next/head';
 
 export default function Head({ title }) {
+   const description = "Easily view workstations and gaming setups from reddit";
+
     return (
       <NextHead>
         <title>{title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={description} />
         <meta property="og:title" content={title} key="title" />
-        <meta property="og:description" content="Easily view workstations and gaming setups from reddit" />
+        <meta property="og:description" content={description} />
         <meta property="og:image" content="https://og-image.vercel.app/OnlySetups.png" />
       </NextHead>
     )
