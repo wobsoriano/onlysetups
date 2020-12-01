b88f95fda73915cb57e39f069922f245a9a587a6
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-12-01 18:29:25 +0800

    update reddit hook

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ module.exports = withPWA({
     pwa: {
         disable: process.env.NODE_ENV === 'development',
         dest: 'public'
+    },
+    images: {
+        domains: ['preview.redd.it']
     }
 });
