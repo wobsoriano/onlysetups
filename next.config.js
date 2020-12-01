4cbe549a1d206d4731f97530b7ec3b1fc5ac8981
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-12-01 11:30:10 +0800

    auto install update

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
     pwa: {
         disable: process.env.NODE_ENV === 'development',
-        dest: 'public',
-        register: false,
-        skipWaiting: false,
-        runtimeCaching
+        dest: 'public'
     }
 });
